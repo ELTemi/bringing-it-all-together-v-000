f838bccd16a57e6101d4aa9e482ebd7b47d544be
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-01 04:06:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -86,7 +86,7 @@ class Dog
       dog = self.new_from_db([dog1[0], dog1[1], dog1[2]])
       #binding.pry
     else
-      dog = self.create(attributes)
+      dog = self.create({name: name, breed: breed})
     end
     dog
     #binding.pry
